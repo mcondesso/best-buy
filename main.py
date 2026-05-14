d1324b38f2d929d4cc1a3b99581a67862c0d69fc
mcondesso <mcondesso@mail.com>
2026-05-14 16:34:12 +0200

    bugfix: missing colon

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from products import Product
 from store import Store
 
 
-def main()
+def main():
     product_list = [Product("MacBook Air M2", price=1450, quantity=100),
                     Product("Bose QuietComfort Earbuds", price=250, quantity=500),
                     Product("Google Pixel 7", price=500, quantity=250),
